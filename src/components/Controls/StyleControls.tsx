a43320aa9e27b4d13b91da1ff98a3d8e1b259000
Omer <27678413+TheOmer77@users.noreply.github.com>
2022-08-29 22:36:01 +0300

    Add color controls

--- src/components/Controls/StyleControls.tsx
+++ src/components/Controls/StyleControls.tsx
@@ -2,6 +2,10 @@ import { Dispatch, SetStateAction } from 'react';
 import LabeledInput from '../general/LabeledInput';
 import type { StyleProperties } from '../../types';
 
+const colorInputStyle = (color: string) => ({
+  boxShadow: `inset 0 -0.25rem 0 0 ${color}`,
+});
+
 const StyleControls = ({
   style,
   setStyle,
@@ -56,6 +60,58 @@ const StyleControls = ({
                 : Number(e.target.value),
           }))
         }
+      />
+      <LabeledInput
+        label='Line color'
+        id='input-color-line'
+        type='text'
+        value={style.colors.line}
+        onChange={e =>
+          setStyle(prev => ({
+            ...prev,
+            colors: { ...prev.colors, line: e.target.value },
+          }))
+        }
+        style={colorInputStyle(style.colors.line)}
+      />
+      <LabeledInput
+        label='Point color'
+        id='input-color-point'
+        type='text'
+        value={style.colors.point}
+        onChange={e =>
+          setStyle(prev => ({
+            ...prev,
+            colors: { ...prev.colors, point: e.target.value },
+          }))
+        }
+        style={colorInputStyle(style.colors.point)}
+      />
+      <LabeledInput
+        label='Temp Line color'
+        id='input-color-tempLine'
+        type='text'
+        value={style.colors.tempLine}
+        onChange={e =>
+          setStyle(prev => ({
+            ...prev,
+            colors: { ...prev.colors, tempLine: e.target.value },
+          }))
+        }
+        style={colorInputStyle(style.colors.tempLine)}
+      />
+      <LabeledInput
+        label='Temp Point color'
+        id='input-color-tempPoint'
+        type='text'
+        value={style.colors.tempPoint}
+        onChange={e =>
+          setStyle(prev => ({
+            ...prev,
+            colors: { ...prev.colors, tempPoint: e.target.value },
+          }))
+        }
+        style={colorInputStyle(style.colors.tempPoint)}
       />
     </>
   );
