53162910f72238f33e0861673d010b9063f16667
Omer <27678413+TheOmer77@users.noreply.github.com>
2022-08-29 13:44:01 +0300

    Remove unused testPolygon from defaults

--- src/constants/defaults.ts
+++ src/constants/defaults.ts
@@ -1,13 +1,3 @@
-import type { Line } from '../types';
-
-export const testPolygon: Line = [
-  [269, 24],
-  [559, 107],
-  [522, 315],
-  [261, 498],
-  [24, 139],
-];
-
 export const spacing = 40;
 
 export const lineWidth = 4;
